aa18992ab1f65a0543619697eddb4f9294817038
can.bueyuekburc.ext <can.bueyuekburc.ext@virtual-solution.com>
2025-08-12 15:23:33 +0200

    update

---
+++ src/main/java/de/buyukburc/springPractice1/SpringPractice1Application.java
@@ -0,0 +1,13 @@
+package de.buyukburc.springPractice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPractice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPractice1Application.class, args);
+	}
+
+}
